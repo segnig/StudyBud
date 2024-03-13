121b6c8bb567d8e8ab3a5972d22c572c52014abc
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-03-13 17:43:26 +0300

    userupdate

--- base/models.py
+++ base/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
+
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [] 
 
 
 class Topic(models.Model):
